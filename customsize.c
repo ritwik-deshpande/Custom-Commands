8d18f76dd50d8ecf957ed9bb81817b384ffc8213
ritwik <ritwikdeshpande01@gmail.com>
2018-09-22 00:09:45 +0530

    Code to get file size in standard block units

---
+++ customsize.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+#include <math.h>
+extern int errno;
+int main(int narg,char** argv)
+{     
+    int fd = open(argv[1], O_RDONLY ); 
+    //printf("%s    %s",argv[1],argv[2]);
+    char a;
+    if (fd ==-1)
+    {
+        // print which type of error have in a code
+        printf("Error Number % d\n", errno); 
+                    
+    }
+    /* fd is file descriptor i.e pointer pointing to the file, 0 is offset from begining of the file and SEEK_BEG is start from beginning there 
+    can be SEEK_CUR SEEK_END*/
+    int sizeoffile = lseek(fd,0,SEEK_END);
+    struct stat fi;
+    stat(argv[1], &fi);
+    int blocksize=fi.st_blksize;
+    /*Offset is 0 + End of file therefore start reading from beginning till this offset i.e no of bytes returned.*/
+    //SEEK_SET from beginnning of the file and SEEK_CUR from current location of the file.
+    printf("%d      %d\n",blocksize,sizeoffile);
+    float m = blocksize/(512);
+    float size = ceil((float)sizeoffile/(float)blocksize)*m;
+    printf("%f\n",size);
+    close(fd);
+    return 0;
+}
